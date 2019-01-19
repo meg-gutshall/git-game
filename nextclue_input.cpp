56a1e50d4039c04630aef96fbd516ecc862a25c2
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-19 12:16:12 -0500

    merge

---
+++ nextclue_input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+//Level 9
+int main()
+{
+    cout << "This file was useless, so I made it better..." << endl;
+    cout << "hope you resolved all your conflicts!!" << endl;
+    return 0;
+}
+
+# Fixed the infinite loop
